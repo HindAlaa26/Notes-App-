b51ff7d6ad4cf179612a01d8bb35ad3855064ab2
HindAlaa26 <hindalaa26.com>
2024-10-19 18:42:06 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      debugShowCheckedModeBanner: false,
+     home: HomeScreen()
+    );
+  }
+}
+
